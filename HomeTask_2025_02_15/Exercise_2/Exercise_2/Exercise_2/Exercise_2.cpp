7ebf7c4030cd3048e5ff808588d297d70fd95444
Валентин Командиров <komandirov.v@mail.ru>
2025-02-15 22:35:56 +0300

    Решение дз 15.02.2025

---
+++ HomeTask_2025_02_15/Exercise_2/Exercise_2/Exercise_2/Exercise_2.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "RU");
+
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    
+    SetConsoleTextAttribute(hConsole, FOREGROUND_RED);
+    cout << "Каждый\n";
+
+    SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_INTENSITY);
+    cout << "\tОхотник\n";
+
+    SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN);
+    cout << "\t\tЖелает\n";
+
+    SetConsoleTextAttribute(hConsole, FOREGROUND_GREEN);
+    cout << "\t\t\tЗнать\n";
+
+    SetConsoleTextAttribute(hConsole, FOREGROUND_GREEN | FOREGROUND_BLUE);
+    cout << "\t\t\t\tГде\n";
+
+    SetConsoleTextAttribute(hConsole, FOREGROUND_BLUE);
+    cout << "\t\t\t\t\tСидит\n";
+
+    SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_BLUE);
+    cout << "\t\t\t\t\t\tФазан\n";
+
+    SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+}
---
+++ HomeTask_2025_02_15/Exercise_3/Exercise_3/Exercise_3/Exercise_3.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+    setlocale(LC_ALL, "RU");
+
+    cout << "\t\t\t\t\t\tПродам дорого друга" << "\n";
+    cout << "\t\t\t\t\t\tпароды двортерьер" << "\n";
+    cout << "\t\t\t\t\t\tTel: 8-800-555-35-35" << "\n";
+    cout << "\t\t\t\t\t\t8\t|8\t|8" << "\n";
+    cout << "\t\t\t\t\t\t800\t|800\t|800" << "\n";\
+    cout << "\t\t\t\t\t\t555\t|555\t|555" << "\n";
+    cout << "\t\t\t\t\t\t35\t|35\t|35" << "\n";
+    cout << "\t\t\t\t\t\t35\t|35\t|35" << "\n";
+    
+    return 0;
+}
+
