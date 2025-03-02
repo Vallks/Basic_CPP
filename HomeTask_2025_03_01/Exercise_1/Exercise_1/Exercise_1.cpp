f550fc3b79212cf902618ba655ca8c707ecff640
Валентин Командиров <komandirov.v@mail.ru>
2025-03-02 15:35:31 +0300

    Добавил упражнения

---
+++ HomeTask_2025_03_01/Exercise_1/Exercise_1/Exercise_1.cpp
@@ -0,0 +1,36 @@
+﻿
+#include <iostream>
+#include <iomanip>
+
+int main()
+{
+	setlocale(LC_ALL, "RU");
+
+	const int SECONDS_DAY = 86400;
+	int seconds_in, hours, minutes, seconds, seconds_before_raschet;
+
+	std::cout << "Введите количество секунд: ";
+
+	std::cin >> seconds_in;
+
+	seconds_in = abs(seconds_in % SECONDS_DAY); //если отрицательное или больше чем день то приведем к нормальному виду
+	seconds_before_raschet = SECONDS_DAY - seconds_in;
+
+	hours = seconds_in / 3600;
+	minutes = seconds_in % 3600 / 60;
+	seconds = seconds_in % 60;
+
+	std::cout << "Сейчас " << std::setfill('0') << std::setw(2) << hours
+				<< ":" << std::setfill('0') << std::setw(2) << minutes
+				<< ":" << std::setfill('0') << std::setw(2) << seconds << std::endl; //манипуляторы для красивого вывода 0 значений
+
+	hours = seconds_before_raschet / 3600;
+	minutes = seconds_before_raschet % 3600 / 60;
+	seconds = seconds_before_raschet % 60;
+
+	std::cout << "До полуночи " << std::setfill('0') << std::setw(2) << hours
+			<< ":" << std::setfill('0') << std::setw(2) << minutes
+			<< ":" << std::setfill('0') << std::setw(2) << seconds << std::endl; //манипуляторы для красивого вывода 0 значений
+
+	return 0;
+}
---
+++ HomeTask_2025_03_01/Exercise_2/Exercise_2/Exercise_2.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <iomanip>
+
+int main()
+{
+	setlocale(LC_ALL, "RU");
+
+	const int SECONDS_WORK_DAY = 3600*8;
+	int seconds_in, hours, minutes, seconds, seconds_before_raschet;
+
+	std::cout << "Введите количество секунд: ";
+
+	std::cin >> seconds_in;
+
+	seconds_in = abs(seconds_in % SECONDS_WORK_DAY); //если отрицательное или больше чем рабочий день то приведем к нормальному виду
+	seconds_before_raschet = SECONDS_WORK_DAY - seconds_in;
+
+	hours = seconds_before_raschet / 3600;
+	minutes = seconds_before_raschet % 3600 / 60;
+	seconds = seconds_before_raschet % 60;
+
+	std::cout << "До конца рабочего дня осталось " << std::setfill('0')	<< std::setw(2) << hours 
+				<< ":" << std::setfill('0')	<< std::setw(2) << minutes 
+				<< ":" << std::setfill('0') << std::setw(2) << seconds << std::endl; //манипуляторы для красивого вывода 0 значений
+
+	return 0;
+}
