b20e9a531f4070cfd5cb5928adb3449a78d709fb
Валентин Командиров <komandirov.v@mail.ru>
2025-03-14 20:44:09 +0300

    Добавил домашку

---
+++ HomeTask_2025_03_13/Exercise_1/Exercise_1/Exercise_1.cpp
@@ -0,0 +1,110 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Ru");
+    double pizza1 = 5.5, pizza2 = 6.5, pizza3 = 7.5, pizza4 = 8.5, pizzaCost = 0, drink1 = 0.5, drink2 = 1.5, drink3 = 2.5, drinkCost = 0;
+    int pizzaInd, drinkInd, pizzaCount, pizzaFreeCount, drinkCount;
+
+    cout << "Выберите пиццу и колчиство через пробел:\n"
+        << "1 - Мартгарита $" << pizza1 << "\n"
+        << "2 - Тропикана $" << pizza2 << "\n"
+        << "3 - 4 сыра $" << pizza3 << "\n"
+        << "4 - Пиперони $" << pizza4 << "\n";
+
+
+    cin >> pizzaInd >> pizzaCount;
+
+    if(pizzaInd < 1 || pizzaInd > 4)
+    {
+        cout << "Вы выбрали не тот товар.";
+        return 1;
+    }
+    if (pizzaCount < 0)
+    {
+        cout << "Вы выбрали не правильное количество.";
+        return 2;
+    }
+
+    cout << "Выберите напиток и колчиство через пробел:\n"
+        << "1 - Кола $" << drink1 << "\n"
+        << "2 - Фанта $" << drink2 << "\n"
+        << "3 - Вятский квас $" << drink3 << "\n";
+
+    cin >> drinkInd >> drinkCount;
+   
+    if (drinkInd < 1 || drinkInd > 3)
+    {
+        cout << "Вы выбрали не тот товар";
+        return 3;
+    }
+    if (drinkCount < 0)
+    {
+        cout << "Вы выбрали не правильное количество.";
+        return 4;
+    }
+
+    cout << "Ваш заказ\n";
+
+    pizzaFreeCount = pizzaCount / 5;
+
+    if (pizzaInd == 1)
+    {
+        pizzaCost = pizzaCount * pizza1 - pizzaFreeCount * pizza1;
+        cout << "Мартгарита - " << pizzaCount << " - $" << pizza1 << endl;
+    }
+    else if (pizzaInd == 2)
+    {
+        pizzaCost = pizzaCount * pizza2 - pizzaFreeCount * pizza2;
+        cout << "Тропикана - " << pizzaCount << " - $" << pizza2 << endl;
+    }
+    else if (pizzaInd == 3)
+    {
+        pizzaCost = pizzaCount * pizza3 - pizzaFreeCount * pizza3;
+        cout << "4 сыра - " << pizzaCount << " - $" << pizza3 << endl;
+    }
+    else
+    {
+        pizzaCost = pizzaCount * pizza4 - pizzaFreeCount * pizza4;
+        cout << "Пиперони - " << pizzaCount << " - $" << pizza4 << endl;
+    }
+
+
+    if (drinkInd == 1)
+    {
+        if (drinkCount > 3)
+        {
+            drinkCost = drink1 * drinkCount * 0.85;
+        }
+        else
+        {
+            drinkCost = drink1 * drinkCount;
+        }
+        cout << "Кола - " << drinkCount << " - $" << drink1 << endl;
+    }
+    else if (drinkInd == 2)
+    {
+        if (drinkCount > 3)
+        {
+            drinkCost = drink2 * drinkCount * 0.85;
+        }
+        else
+        {
+            drinkCost = drink2 * drinkCount;
+        }
+        cout << "Фанта - " << drinkCount << " - $" << drink2 << endl;
+    }
+    else
+    {
+        drinkCost = drink3 * drinkCount * 0.85; // Поскольку заранее знаем цену сразу применил скидку
+        cout << "Вятский квас - " << drinkCount << " - $" << drink3 << endl;
+    }
+
+   
+    cout << "Итого к оплате: " << ((pizzaCost + drinkCost) > 50.0?(pizzaCost + drinkCost)*0.8:(pizzaCost + drinkCost));
+
+     return 0;
+}
+
---
+++ HomeTask_2025_03_13/Exercise_2/Exercise_2/Exercise_2.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+
+using namespace std;
+
+double getSal(double prod) // лень писать 3 раза ифа
+{
+    if (prod < 500)
+    {
+        return 200 + prod * 0.03;
+    }
+    else if(prod < 1000)
+    {
+        return 200 + prod * 0.05;
+    }
+    else
+    {
+        return 200 + prod * 0.08;
+    }
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Ru");
+
+    double prod1, prod2, prod3, sal1, sal2, sal3;
+
+    cout << "Введите продажи менеджеров через пробел: ";
+    cin >> prod1 >> prod2 >> prod3;
+
+    if (prod1 < 0 || prod2 < 0 || prod3 < 0)
+    {
+        cout << "Не правильно ввели продажи!";
+        return 1;
+    }
+    
+    sal1 = getSal(prod1);
+    sal2 = getSal(prod2);
+    sal3 = getSal(prod3);
+
+    if (prod1 > prod2 && prod1 > prod3)
+    {
+        sal1 += 200;
+    }
+    if (prod2 > prod1 && prod2 > prod3)
+    {
+        sal2 += 200;
+    }
+    if (prod3 > prod2 && prod3 > prod1)
+    {
+        sal3 += 200;
+    }
+    // если у кого-то равны то ни кто не получит
+    
+    cout << "Зарплата менеджера 1 = " << sal1 << endl
+        << "Зарплата менеджера 2 = " << sal2 << endl
+        << "Зарплата менеджера 3 = " << sal3 << endl;
+
+    return 0;
+}
