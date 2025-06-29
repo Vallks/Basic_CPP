128887aff4dbca1f2c3b28519c09b4e8e90e1ffb
Валентин Командиров <komandirov.v@mail.ru>
2025-06-29 23:25:43 +0300

    Добавил упражнений

---
+++ HomeTask_2025_06_21/Exercise_2/Exercise_2/Exercise_2.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <cstring>
+#include <cctype> 
+using namespace std;
+
+void myFunk(char* str)
+{
+	int res = 0;
+	bool flag = false;
+
+	for (int i = 0; str[i] != '\0'; i++) {
+		if (isspace(str[i])) {
+			flag = false;
+		}
+		else if (!flag) {
+			flag = true;
+			res++;
+		}
+	}
+
+	cout << res << " слов";
+	return;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "RU");
+
+	const int SIZE = 256;
+	char str[SIZE];
+	int indx;
+
+	cout << "Введите строку: ";
+	cin.getline(str, SIZE);
+
+	myFunk(str);
+
+	return 0;
+}
