e03de1e3c63dade115b0a5d87dc950fec385e301
Валентин Командиров <komandirov.v@mail.ru>
2025-07-02 20:02:02 +0300

    Добавил упражнения

---
+++ HomeTask_2025_06_28/Exercise_1/Exercise_1/Exercise_1.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+using namespace std;
+
+void initArr(int* arr, const int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		arr[i] = i * i;
+	}
+	return;
+}
+
+void printArr(int* arr, const int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		cout << arr[i] << '\t';
+	}
+	cout << '\n';
+	return;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "RU");
+
+	unsigned int size;
+	cout << "Введите размер массива: ";
+	cin >> size;
+
+	int* arr = new int[size];
+	initArr(arr, size);
+	printArr(arr, size);
+
+	int sum = 0;
+	for (int i = 0; i < size; i++)
+	{
+		sum += arr[i];
+	}
+	cout << sum;
+	return 0;
+}
+
